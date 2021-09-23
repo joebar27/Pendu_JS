42931185f3c4049087568d1ecb6434f5b3991c01
Anger Romain <anger.romain@gmail.com>
2021-09-24 00:03:02 +0200

    fonction de génération du mot et son remplacement par des underscores

--- js/pendu.js
+++ js/pendu.js
@@ -1,22 +1,26 @@
 //----------------------DECLARATION DES VARIABLE-----------------------------------
 var interfaceChoice;
 var interfaceCheck;
+var wordOfDico = ["stylo", "chien", "chat", "ordinateur", "verre", "chocolat", "bougie", "cahier"];
+var wordSplited;
+const initScore = 7;
+var currentScore;
+var currentWord;
+var longWord;
+var wordHide = [];
 
 //------------------------CREATION DES FONCTIONS-----------------------------------
-alert("Bienvenue sur le jeu du pendu");
-menu();
 
 function menu() {
   // capture du choix de l'utilisateur dans le menu de lancement.
   interfaceChoice = prompt("Merci de saisir : \n\"J\" pour jouer \"Q\" pour quitter \"R\" pour obtenir les règles");
   interfaceCheck = interfaceChoice.toLocaleLowerCase();
-  console.log(interfaceChoice);
   // recursivité de l'interface en cas de mauvaise entrer.
   if (interfaceCheck !== "j" && interfaceCheck !== "r" && interfaceCheck !== "q") {
     return menu();
   }
   // lancement du jeu par le choix utilisateur.
-  if (interfaceCheck === "j") {
+  else if (interfaceCheck === "j") {
     return gameStart();
   }
   // affiche les regles du jeu par le choix utilisateur.
@@ -31,6 +35,44 @@ function menu() {
 }
 
 function gameStart() {
-    
+  currentScore = initScore;
+  console.log(currentScore);
+  wordChoice();
+  replaceWord(currentWord);
+}
+
+function wordChoice() {
+  let randWord = Math.floor(Math.random() * wordOfDico.length);
+  currentWord = wordOfDico[randWord];
+  return currentWord;
+}
+
+function replaceWord() {
+  wordSplited = currentWord.split('');
+  console.log(wordSplited);
+  let longWord = currentWord.length;
+  while (wordHide.length !== longWord) {
+    wordHide.push("_");
+  }
+  console.log(wordHide);
+  alert(`le mot choisit par l'ordinateur est :\n\n ${wordHide.join('  ')}`)
+  return wordHide.join(' ');
+}
+
+function wordCompared() {
 
 }
+
+function userScore() {
+  if (wordCompared() === false) {
+    currentScore -= 1;
+  }
+}
+
+
+
+
+//-------------------------DEROULEMENT DU JEU---------------------------------------
+
+alert("Bienvenue sur le jeu du pendu");
+menu();
