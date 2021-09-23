d12caa69039f7272a5ec069f3ba5020933e89542
Anger Romain <anger.romain@gmail.com>
2021-09-23 19:53:24 +0200

    interface de commande terminé

--- js/pendu.js
+++ js/pendu.js
@@ -1,23 +1,36 @@
 //----------------------DECLARATION DES VARIABLE-----------------------------------
-var interface;
-
+var interfaceChoice;
+var interfaceCheck;
 
 //------------------------CREATION DES FONCTIONS-----------------------------------
 alert("Bienvenue sur le jeu du pendu");
+menu();
 
-interface = prompt("Merci de saisir : \n\"J\" pour jouer \"Q\" pour quitter \"R\" pour obtenir les règles");
-//interface.toLocaleLowerCase = interfaceCheck;
-
-if (interface.toLocaleLowerCase !== "j" && interface.toLocaleLowerCase !== "r" && interface.toLocaleLowerCase !== "q"){
-    console.log(interfaceCheck);
-    return interfaceCheck; 
-}
-if (interfaceCheck === "j"){
-    return console.log(interfaceCheck);
-}
-else if (interfaceCheck === "r"){
-    return alert("Voici les règles:\nEntrer une lettre unique que vous pensé etre correct au mot choisit par l'ordinateur\nVous avez 7 tentative avant d'etre pendu\nBon jeu.");
-}
-else if (interfaceCheck === "q"){
+function menu() {
+  // capture du choix de l'utilisateur dans le menu de lancement.
+  interfaceChoice = prompt("Merci de saisir : \n\"J\" pour jouer \"Q\" pour quitter \"R\" pour obtenir les règles");
+  interfaceCheck = interfaceChoice.toLocaleLowerCase();
+  console.log(interfaceChoice);
+  // recursivité de l'interface en cas de mauvaise entrer.
+  if (interfaceCheck !== "j" && interfaceCheck !== "r" && interfaceCheck !== "q") {
+    return menu();
+  }
+  // lancement du jeu par le choix utilisateur.
+  if (interfaceCheck === "j") {
+    return gameStart();
+  }
+  // affiche les regles du jeu par le choix utilisateur.
+  else if (interfaceCheck === "r") {
+    alert("Voici les règles:\nEntrer une lettre unique que vous pensé etre correct au mot choisit par l'ordinateur\nVous avez 7 tentative avant d'etre pendu\nBon jeu.");
+    return menu();
+  }
+  // affiche une confirmation de l'arret du jeu.
+  else if (interfaceCheck === "q") {
     return alert("Merci d'avoir joué\nA bientot.");
+  }
+}
+
+function gameStart() {
+    
+
 }
