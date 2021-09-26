387d4bd8f0ed3465ef9764cd66d0b169d7bcb891
Anger Romain <anger.romain@gmail.com>
2021-09-26 12:21:40 +0200

    ajout de la liste des prénoms

--- js/pendu.js
+++ js/pendu.js
@@ -1,25 +1,39 @@
 //----------------------DECLARATION DES TABLEAUX DE MOT-----------------------------------
 
-var wordListGlobal = ["stylo", "chien", "chat", "ordinateur", "verre", "chocolat", "bougie", "cahier", "anticonstitutionnellement","Palourde","Creux","Faire","Siècle","Olive","Cou","Comte",
-"Bluff","Bronze","Claustrophobie","Amputer","Somnambule","Exercice","Modifier","Benne","Longue","Scientifique","Tournesol","Marginal","Russie","Esquimau","Clavier","Pouls","Joyau",
-"Fondue","Pipeline","Pauvres","Dent","Fausser","Analytique","Hormones","Dépression","Jump","Dupliquer","Urgence","Concubines","Bars","Pieds","Vol","JeudiBordeaux","Sourcils","Oreille",
-"Ovale","Beffroi","Cintre","Tube","alade","Saveur","Boulangerie","Swing","Voler","Pistolet","Nez","Doublure","Monstre","Ventiler","Baigner","Pratique","Objectifs","Clavier","Diamant",
-"Etriers","Falsifier","Cerceau","Cygne","Parieur","Roues","Coma","Coeur","Abdomen","Doigt","Viande","Muscle","Fesses","Bouche","Cou","Corps","Visage","REVOYEZ","CONTENT","AFGHANS","ASSYRIE","RAIMOND",
-"BATEAUX","HAUTAIN","MOQUONS","TRAPPES","TOMBEUR","BOUTENT","INFOUTU","NOMMAIT","VITESSE","OCCULUS","LAVIGNE","ULCERES","HUILEUX","EPOPEES","VENTEUX","RIDEAUX","BLAGUER","FRELONS","SLALOMS","COUDERT",
-"TRANSFO","SULFATE","REMPLIE","ALCALIN","BARRIER","ASSAGIT","NIRVANA","LARMOIE","TORRIDE","LISSAGE","ATTISEE","ETRIPER","SIDEREE","REMOISE","TONDENT","DELARUE","ACTUELS","ASSURES","FANCHON","SERVIER",
-"NASALES","LECOMTE","VEAUTER","BRISEES","SATURER","ALLEGES","DUNNING","HOMELIE","QUINTAL","DIVERSE","REELIRE","CORINNE","SONDIER","RENOUEE","COLLEES","BAISAGE","BIDOCHE","PAPESSE","SURPAYE","UPVOTEE",
-"MIXTURE","INFORMA","NASALES","EMETTRE","CANTONS","ROMANDE","MORAINE","DANSEUR","REFERME","FLUVIAL","DENIENT","SPOILEZ","PRETERA","EMETTRA","ERIGNAC","AUDITER","CIRAGES","NOTAIRE","POTIONS","ENROBER",
-"PLIABLE","RECALER","TANTALE","ROSETTE","JEUNENT","ECZEMAS","NAVIRES","DEROBES","EXILANT","ANNULEE","ISABEAU","OFFRIRA","RISQUEZ","TUTORES","LAURENS",];
+var wordListGlobal = ["stylo", "chien", "chat", "ordinateur", "verre", "chocolat", "bougie", "cahier", "anticonstitutionnellement", "Palourde", "Creux", "Faire", "Siècle", "Olive", "Comte", "Bluff", "Bronze",
+  "Claustrophobie", "Amputer", "Somnambule", "Exercice", "Modifier", "Benne", "Longue", "Scientifique", "Tournesol", "Marginal", "Russie", "Esquimau", "Pouls", "Joyau", "Fondue", "Pipeline", "Pauvres", "Dent", "Fausser",
+  "Analytique", "Hormones", "Dépression", "Dupliquer", "Urgence", "Concubines", "Bars", "Pieds", "Vol", "JeudiBordeaux", "Sourcils", "Oreille", "Ovale", "Beffroi", "Cintre", "Tube", "alade", "Saveur", "Boulangerie", "Swing",
+  "Voler", "Pistolet", "Doublure", "Monstre", "Ventiler", "Baigner", "Pratique", "Objectifs", "Clavier", "Diamant", "Etriers", "Falsifier", "Cerceau", "Parieur", "Roues", "Coma", "Coeur", "Abdomen", "Doigt",
+  "Muscle", "Fesses", "Cou", "Corps", "Visage", "Revoyer", "Content", "Afghans", "Assyrie", "Raimond", "Bateaux", "Hautain", "Moquons", "Trappes", "Tombeur", "Boutent", "Infoutu", "Nommait", "Vitesse", "Oocculus", "Lavigne",
+  "Ulceres", "Huileux", "Epopees", "Venteux", "Rideaux", "Blaguer", "Frelons", "Slaloms", "Coudert", "Transfo", "Sulfate", "Remplie", "Alcalin", "Barrier", "Assagit", "Nirvana", "Larmoie", "Torride", "Lissage", "Attisee", "Etriper",
+  "Sideree", "Remoise", "Tondent", "Delarue", "Actuels", "Assures", "Fanchon", "Servier", "Nasales", "Lecompte", "Veauter", "Brisees", "Saturer", "Alleges", "Dunning", "Homelie", "Quintal", "Diverse", "Reelire", "Sondier", "Renouee",
+  "Collees", "Baisage", "Bidoche", "Papesse", "Surpaye", "Upvotee", "Mixture", "Informatique", "Emettre", "Cantons", "Romande", "Moraine", "Danseur", "Referme", "Fluvial", "Denient", "Spoilez", "Pretera", "Emettra", "Erignac",
+  "Aauditer", "Cirages", "Notaire", "Potions", "Enrober", "Pliable", "Recaler", "Tantale", "Rosette", "Jeunent", "Eczemas", "Navires", "Derobes", "Exilant", "Annulee", "Isabeau", "Offrira", "Risquez", "Tutores", "Laurens", "Escargot",
+  "Poney", "Thon", "Gorille", "Méduse", "Autruche", "Langouste", "Chien", "Crocodile", "Girafe", "Mouflon", "Oursin", "Tortue", "Mouton", "Chinchilla", "Ours", "Faucon", "Souris", "Serpent", "Chameau", "Iguane", "Oie",
+  "Pie", "Cerf", "Sauterelle", "Papillon", "Sanglier", "Crabe", "Mollusque", "Colombe", "Mule", "Hippopotame", "Lion", "Corbeau", "Kangourou", "Moustique", "Perroquet", "Scarabée", "Aigle", "Faisan", "Vache", "Calmar", "Singe",
+  "Koala", "Paresseux", "Cygne", "Anneau", "Noir", "Loup", "Dix", "Livre", "Sol", "Pomme", "Marguerite", "Poussin", "Jump", "Pantalon", "Montagne", "Requin", "Grand", "Films", "Vendeur", "Fiche", "Bouche", "Moto", "Sucette",
+  "Loupe", "Panda", "Ventre", "Fleur", "Lait", "Professeur", "Poisson", "Marteau", "Taxi", "Tigre", "Chaussettes", "Cuisinier", "Feuilles", "Lit", "Sandwich", "Herbe", "Poire", "Couteau", "Argent", "Nez", "Malade", "Craie", "Trois",
+  "Lanterne", "Automne", "Grain", "raisin", "Neige", "Nappe", "Rire", "Chat", "Casque", "Pain", "Dauphin", "Vert", "Jardin", "Chapeau", "Viande", "Quatre", "Vache", "Lune", "Verre", "Chocolat", "Poulet", "DessusCintre", "Main", "Triangle",
+  "Baignoire", "Boisson"
+];
 
-var wordListAnimaux = ["Escargot","Poney","Thon","Gorille","Voler","Méduse","Autruche","Langouste","Chien","Crocodile","Girafe","Oie","Mouflon","Oursin","Tortue","Mouton","Chinchilla","Ours","Faucon","Souris",
-  "Serpent","Chameau","Iguane","Oie","Pie","Cerf","Sauterelle","Papillon","Sanglier","Crabe","Mollusque","Colombe","Mule","Hippopotame","Lion","Corbeau","Kangourou","Moustique","Perroquet","Scarabée","Loup","Aigle",
-  "Faisan","Vache","Calmar","Singe","Koala","Paresseux","Cygne"];
+var wordListAnimaux = ["Escargot", "Poney", "Thon", "Gorille", "Voler", "Méduse", "Autruche", "Langouste", "Chien", "Crocodile", "Girafe", "Oie", "Mouflon", "Oursin", "Tortue", "Mouton", "Chinchilla", "Ours", "Faucon", "Souris",
+  "Serpent", "Chameau", "Iguane", "Oie", "Pie", "Cerf", "Sauterelle", "Papillon", "Sanglier", "Crabe", "Mollusque", "Colombe", "Mule", "Hippopotame", "Lion", "Corbeau", "Kangourou", "Moustique", "Perroquet", "Scarabée", "Loup", "Aigle",
+  "Faisan", "Vache", "Calmar", "Singe", "Koala", "Paresseux", "Cygne"
+];
 
-var wordListEnfant = ["Anneau","Noir","Loup","Dix","Livre","Sol","Pomme","Marguerite","Poussin","Jump","Pantalon","Montagne","Requin","Grand","Films","Vendeur","Fiche","Bouche","Girafe","Moto","Sucette","Loupe",
-  "Panda","Ventre","Fleur","Lait","Professeur","Poisson","Marteau","Taxi","Tigre","Chaussettes","Cuisinier","Feuilles","Lit","Sandwich","Herbe","Poire","Couteau","Argent","Nez","Malade","Craie","Trois","Lanterne",
-  "Automne","Grain","raisin","Neige","Nappe","Rire","Chat","Casque","Pain","Dauphin","Vert","Jardin","Chapeau","Viande","Quatre","Vache","Lune","Verre","Chocolat","Poulet","DessusCintre","Main","Triangle","Baignoire",
-  "Boisson"];
+var wordListEnfant = ["Anneau", "Noir", "Loup", "Dix", "Livre", "Sol", "Pomme", "Marguerite", "Poussin", "Jump", "Pantalon", "Montagne", "Requin", "Grand", "Films", "Vendeur", "Fiche", "Bouche", "Girafe", "Moto", "Sucette", "Loupe",
+  "Panda", "Ventre", "Fleur", "Lait", "Professeur", "Poisson", "Marteau", "Taxi", "Tigre", "Chaussettes", "Cuisinier", "Feuilles", "Lit", "Sandwich", "Herbe", "Poire", "Couteau", "Argent", "Nez", "Malade", "Craie", "Trois", "Lanterne",
+  "Automne", "Grain", "raisin", "Neige", "Nappe", "Rire", "Chat", "Casque", "Pain", "Dauphin", "Vert", "Jardin", "Chapeau", "Viande", "Quatre", "Vache", "Lune", "Verre", "Chocolat", "Poulet", "DessusCintre", "Main", "Triangle",
+  "Baignoire", "Boisson"
+];
 
+var wordListePrenom = ["Romain", "Axel", "Remi", "Hugo", "Laurina", "Anais", "David", "Thomas", "Paul", "Arnold", "Jules", "Lucas", "Lise", "Laura", "Jean", "Claudine", "Pascal", "Corinne", "Theo", "Pierre", "Camille", "Arthur",
+  "Evan", "Clara", "Emma", "Charlotte", "Adrien", "Celia", "Eva", "Alexis", "Antoine", "Adam", "Alexandre", "Ambre", "Baptiste", "Clemence", "Enzo", "Juliette", "Lena", "Lea", "Jeanne", "Julie", "Julien", "Leo", "Matteo", "Maeva", "Mael",
+  "Mathilde", "Maxime", "Louise", "Lucie", "Manon", "Gabriel", "Noemie", "Raphael", "Quentin", "Jade", "Ines", "Sarah", "Tom", "Nicolas", "Lola", "Louis", "Mathis", "Nathan", "Oceane", "Pauline", "Romane", "Zoe", "Valentin", "Ethan",
+  "Kylian", "Lina", "Lisa", "Maelys", "Matheo", "Aaron", "Alicia", "Andrea", "Antonin", "Audrey", "Angele", "Anthony", "Bastien", "Adele", "Alan", "Alexia", "Amandine", "Angelina", "Aymeric", "Bryan", "Claire", "Coralie", "Elsa",
+  "Agathe", "Charles", "Constance", "Elias", "Dorian", "Dylan", "Eleonore", "Elisa", "Elodie", "Fanny", "Alice", "Anna", "Apolline", "Augustin", "Candice", "Charline", "Elise", "Emilie", "Alban", "Amelie", "Aurelien"
+];
 //-----------------------------------------------------------------DECLARATION DES VARIABLE--------------------------------------------------------------------------
 
 var interfaceChoice;
@@ -76,30 +90,37 @@ function themechoice() {
   askPlayerTheme = prompt(`Voulez vous choisir un thème en particulier ?
     - Vous pouvez choisir des mots Généraux en tapant : 'G'
     - Vous pouvez choisir des mots sur les Animaux en tapant : 'A'
-    - Vous pouvez choisir des mots pour les Enfants en tapant : 'E'`);
-    askPlayerTheme = askPlayerTheme.toLocaleLowerCase();
-  switch (askPlayerTheme){
-    case "a" : generateWordAnimaux();
-    break;
-    case "g" : generateWordGlobal();
-    break;
-    case "e" : generateWordEnfant();
-    break;
-    default : themechoice();
-    break;
+    - Vous pouvez choisir des mots pour les Enfants en tapant : 'E'
+    - Vous pouvez choisir des mots sur les Prénoms en tapant : 'P'`);
+  askPlayerTheme = askPlayerTheme.toLocaleLowerCase();
+  switch (askPlayerTheme) {
+    case "a":
+      generateWordAnimaux();
+      break;
+    case "g":
+      generateWordGlobal();
+      break;
+    case "e":
+      generateWordEnfant();
+      break;
+    case "p":
+      generateWordPrenom();
+      break;
+    default:
+      themechoice();
+      break;
   }
 }
 // Fonction pour le déroulement du jeu
 function gamePlay() {
-  while (currentScore !== 0 && hiddenWord.join('') !== currentWord) {//
+  while (currentScore !== 0 && hiddenWord.join('') !== currentWord) { //
     letterCaptured();
     letterCompared();
     userScore();
   }
   if (currentScore === 0) {
     gameOver();
-  }
-  else{
+  } else {
     alert("vous avez trouver le mot");
   }
   return menu();
@@ -122,6 +143,13 @@ function generateWordAnimaux() {
 // Fonction pour la génération aléatoire du mot choisi par l'ordinateur parmi le tableau de mot : enfant
 function generateWordEnfant() {
   currentWord = wordListEnfant[Math.floor(Math.random() * wordListEnfant.length)];
+  console.log(currentWord);
+  currentWord = currentWord.toLocaleLowerCase();
+  return currentWord;
+}
+// Fonction pour la génération aléatoire du mot choisi par l'ordinateur parmi le tableau de mot : enfant
+function generateWordPrenom() {
+  currentWord = wordListePrenom[Math.floor(Math.random() * wordListePrenom.length)];
   console.log(currentWord);
   currentWord = currentWord.toLocaleLowerCase();
   return currentWord;
@@ -154,7 +182,7 @@ function letterCompared() {
     if (wordSplited[i] === currentLetter) {
       hiddenWord[i] = currentLetter;
       letterFind = true;
-    } 
+    }
   }
   return letterFind;
 }
@@ -166,8 +194,8 @@ function gameOver() {
 }
 
 // Fonction de décrémentation du score de l'utilisateur
-function userScore(){
-  if (letterFind === false){
+function userScore() {
+  if (letterFind === false) {
     currentScore -= 1;
   }
   return currentScore;
